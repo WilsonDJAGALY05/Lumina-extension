5877cb7d44dfd4d0aca5030238d7e9d06abe59d6
WilsonDJAGALY05 <wilsondjagaly8@gmail.com>
2025-06-15 11:28:03 +0100

    Fix: Remove unused openai dependency and redundant Navbar/Footer imports

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,15 +6,12 @@ import { EmailGenerator } from '../components/EmailGenerator'
 import { TemplateManager } from '../components/TemplateManager'
 import { Analytics } from '../components/Analytics'
 import { EmailIntegrations } from '../components/EmailIntegrations'
-import { Navbar } from '../components/Navbar'
-import { Footer } from '../components/Footer'
 
 export default function Home() {
   const { colorMode } = useColorMode()
 
   return (
     <Box minH="100vh" bg={colorMode === 'dark' ? 'gray.900' : 'gray.50'}>
-      <Navbar />
       <Container maxW="container.xl" py={8}>
         <VStack spacing={8} align="stretch">
           <EmailGenerator onGenerate={(email) => console.log(email)} />
@@ -23,7 +20,6 @@ export default function Home() {
           <EmailIntegrations />
         </VStack>
       </Container>
-      <Footer />
     </Box>
   )
 } 
